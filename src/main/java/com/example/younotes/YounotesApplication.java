5e7b79990502c4afb4047caf09657df80852ab67
CliffMaz <cliff.mz18@gmail.com>
2021-12-31 00:39:12 +0200

    database fix

--- src/main/java/com/example/younotes/YounotesApplication.java
+++ src/main/java/com/example/younotes/YounotesApplication.java
@@ -30,7 +30,7 @@ public class YounotesApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://younotes-app-1.herokuapp.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
@@ -47,7 +47,7 @@ public class YounotesApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("https://younotes-app-1.herokuapp.com");
 			}
 		};
 	}
